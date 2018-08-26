a8847cb641553db34af6d79937a2d44dcda86e94
Ashkan Kiani <ashkan@zephyr>
2018-08-25 20:42:01 -0700

    Add command to launch with keyboard shortcut

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,20 +1,19 @@
-
 // Show on all pages
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    chrome.pageAction.show(tabId);
+  chrome.pageAction.show(tabId);
 });
 
 // Send current tabs url to MPV server
-chrome.pageAction.onClicked.addListener(function(tab){
-    // console.info("Clicked!", tab.url)
-    playUrl(tab.url);
+chrome.pageAction.onClicked.addListener(function(tab) {
+  // console.info("Clicked!", tab.url)
+  playUrl(tab.url);
 });
 
 function playUrl(url) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = handleXHR;
-    xhr.open("GET", "http://localhost:7531/?play_url="+url, true);
-    xhr.send();
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = handleXHR;
+  xhr.open("GET", "http://localhost:7531/?play_url=" + url, true);
+  xhr.send();
 }
 
 function handleXHR() {
@@ -22,15 +21,25 @@ function handleXHR() {
 }
 
 var parent = chrome.contextMenus.create({
-	"id": "thann.play-with-mpv",
-	"title": "Play with MPV",
-	"contexts": ["page", "link", "video", "audio"]
+  id: "thann.play-with-mpv",
+  title: "Play with MPV",
+  contexts: ["page", "link", "video", "audio"]
 });
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
-	// console.log("item " + info.menuItemId + " was clicked");
-	// console.log("info: " + JSON.stringify(info));
-	// console.log("tab: " + JSON.stringify(tab));
+  // console.log("item " + info.menuItemId + " was clicked");
+  // console.log("info: " + JSON.stringify(info));
+  // console.log("tab: " + JSON.stringify(tab));
 
-	playUrl(info["linkUrl"] || info["srcUrl"]|| info["pageUrl"]);
+  playUrl(info["linkUrl"] || info["srcUrl"] || info["pageUrl"]);
+});
+
+chrome.commands.onCommand.addListener(function(command) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    // since only one tab should be active and in the current window at once
+    // the return variable should only have one entry
+    var activeTab = tabs[0];
+    var activeTabId = activeTab.id; // or do whatever you need
+    playUrl(tab.url);
+  });
 });
